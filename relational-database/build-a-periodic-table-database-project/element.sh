b56b755e3fd813139a0ddb4d76fdf8c60a3d186f
Vien Dinh <maxvien@gmail.com>
2024-08-02 17:25:53 +0700

    refactor: build-a-periodic-table-database-project

--- relational-database/build-a-periodic-table-database-project/element.sh
+++ relational-database/build-a-periodic-table-database-project/element.sh
@@ -2,28 +2,30 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-ELEMENTS_QUERY="select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements left join properties using(atomic_number) left join types using(type_id)"
+ELEMENTS_QUERY="select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements inner join properties using(atomic_number) inner join types using(type_id)"
 
 PRINT_ELEMENT() {
   if [[ $ELEMENT ]]
   then
-    echo $1 | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS
+    echo $ELEMENT | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS
     do
-      echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."  
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
     done
-  else 
+  else
     echo "I could not find that element in the database."
   fi
 }
 
-if [[ ! $1 ]]
+if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-elif [[ $1 =~ ^[0-9]+$ ]]
-then
-  ELEMENT=$($PSQL "$ELEMENTS_QUERY where atomic_number=$1")
-  PRINT_ELEMENT "$ELEMENT"
 else
-  ELEMENT=$($PSQL "$ELEMENTS_QUERY where symbol='$1' or name='$1'")
-  PRINT_ELEMENT "$ELEMENT"
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "$ELEMENTS_QUERY where atomic_number=$1")
+    PRINT_ELEMENT
+  else
+    ELEMENT=$($PSQL "$ELEMENTS_QUERY where symbol='$1' or name='$1'")
+    PRINT_ELEMENT
+  fi
 fi
