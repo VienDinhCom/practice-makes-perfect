658d21e75c3bcb400d8f732980e05436e58bc9bc
Vien Dinh <maxvien@gmail.com>
2024-10-22 16:40:50 +0700

    coding-interview-prep/algorithms/pairwise

---
+++ coding-interview-prep/algorithms/05-implement-bubble-sort.js
@@ -0,0 +1,26 @@
+import { expect } from 'jsr:@std/expect';
+
+function bubbleSort(arr = []) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = arr.length - 1; j > i; j--) {
+      if (arr[j] < arr[j - 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+const r = bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+
+console.log(r);
+
+Deno.test('test', () => {
+  const input = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+  const output = input.toSorted((a, b) => a - b);
+
+  expect(bubbleSort(input)).toStrictEqual(output);
+});
