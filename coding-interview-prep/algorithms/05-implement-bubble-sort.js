9e9fe521e235dae803c86a3f5e37ba899659d691
Vien Dinh <maxvien@gmail.com>
2024-10-24 15:28:42 +0700

    ok

--- coding-interview-prep/algorithms/05-implement-bubble-sort.js
+++ coding-interview-prep/algorithms/05-implement-bubble-sort.js
@@ -6,9 +6,9 @@ function bubbleSort(arr = []) {
   for (let i = 0; i < arr.length; i++) {
     swapped = false;
 
-    for (let j = arr.length - 1; j > i; j--) {
-      if (arr[j] < arr[j - 1]) {
-        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 
         swapped = true;
       }
@@ -19,10 +19,6 @@ function bubbleSort(arr = []) {
 
   return arr;
 }
-
-const r = bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
-
-console.log(r);
 
 Deno.test('test', () => {
   const input = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
