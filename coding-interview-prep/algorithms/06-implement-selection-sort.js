f61886594a8d3175c7c7d079069da960ce8fe0a8
Vien Dinh <maxvien@gmail.com>
2024-10-24 15:32:35 +0700

    ok

---
+++ coding-interview-prep/algorithms/06-implement-selection-sort.js
@@ -0,0 +1,10 @@
+function selectionSort(array = []) {
+  return array;
+}
+
+Deno.test('test', () => {
+  const input = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+  const output = input.toSorted((a, b) => a - b);
+
+  expect(selectionSort(input)).toStrictEqual(output);
+});
