4bb469f8199d3b64621f6b79f82cfad69a6e66f3
Vien Dinh <maxvien@gmail.com>
2024-10-25 21:03:57 +0700

    ok

--- coding-interview-prep/algorithms/06-implement-selection-sort.ts
+++ coding-interview-prep/algorithms/06-implement-selection-sort.ts
@@ -1,25 +1,14 @@
 import { expect } from 'jsr:@std/expect';
 
-function insertionSort(arr: number[]) {
-  for (let i = 0; i < arr.length; i++) {
-    let minj = i;
-
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minj]) {
-        minj = j;
-      }
-    }
-
-    if (minj !== i) {
-      [arr[i], arr[minj]] = [arr[minj], arr[i]];
-    }
-  }
-
+function selectionSort(arr: number[]) {
   return arr;
 }
 
+// https://visualgo.net/en/sorting
+// https://www.w3schools.com/dsa/dsa_algo_selectionsort.php
+
 Deno.test('test', () => {
-  const input = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+  const input = [3, 15, 7, 1, 12, 19, 5, 8, 2, 10];
   const output = input.toSorted((a, b) => a - b);
 
   expect(selectionSort(input)).toStrictEqual(output);
