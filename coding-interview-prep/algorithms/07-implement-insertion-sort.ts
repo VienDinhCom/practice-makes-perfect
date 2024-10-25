e8dbe042072b4a36f4578c6c4f9eae0025d703e9
Vien Dinh <maxvien@gmail.com>
2024-10-25 09:30:28 +0700

    add resources

--- coding-interview-prep/algorithms/07-implement-insertion-sort.ts
+++ coding-interview-prep/algorithms/07-implement-insertion-sort.ts
@@ -24,6 +24,8 @@ function insertionSort(arr: number[]): number[] {
   return arr;
 }
 
+// https://www.w3schools.com/dsa/dsa_algo_insertionsort.php
+
 Deno.test('test', () => {
   const input = [3, 15, 7, 1, 12, 19, 5, 8, 2, 10];
   const output = input.toSorted((a, b) => a - b);
