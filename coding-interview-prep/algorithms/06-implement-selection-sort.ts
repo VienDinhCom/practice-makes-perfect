4a2dd001bc75a205fab020926e1f98990dcb3af8
Vien Dinh <maxvien@gmail.com>
2024-10-26 16:22:20 +0700

    ok

--- coding-interview-prep/algorithms/06-implement-selection-sort.ts
+++ coding-interview-prep/algorithms/06-implement-selection-sort.ts
@@ -1,7 +1,19 @@
 import { expect } from 'jsr:@std/expect';
 
 function selectionSort(arr: number[]) {
-  
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
 
   return arr;
 }
