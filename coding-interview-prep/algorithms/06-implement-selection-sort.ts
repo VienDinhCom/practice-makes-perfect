5aa0129199d7c623b6af243b6db42579b1c5ff66
Vien Dinh <maxvien@gmail.com>
2024-10-29 09:31:21 +0700

    test

--- coding-interview-prep/algorithms/06-implement-selection-sort.ts
+++ coding-interview-prep/algorithms/06-implement-selection-sort.ts
@@ -23,9 +23,44 @@ function selectionSort(nums: number[]): number[] {
 // https://visualgo.net/en/sorting
 // https://www.w3schools.com/dsa/dsa_algo_selectionsort.php
 
-Deno.test('test', () => {
+Deno.test('standard array', () => {
   const input = [3, 15, 7, 1, 12, 19, 5, 8, 2, 10];
   const output = input.toSorted((a, b) => a - b);
+  expect(selectionSort(input)).toStrictEqual(output);
+});
 
+Deno.test('empty array', () => {
+  const input: number[] = [];
+  const output: number[] = [];
+  expect(selectionSort(input)).toStrictEqual(output);
+});
+
+Deno.test('single element', () => {
+  const input = [42];
+  const output = [42];
+  expect(selectionSort(input)).toStrictEqual(output);
+});
+
+Deno.test('already sorted array', () => {
+  const input = [1, 2, 3, 4, 5];
+  const output = [1, 2, 3, 4, 5];
+  expect(selectionSort(input)).toStrictEqual(output);
+});
+
+Deno.test('reverse sorted array', () => {
+  const input = [5, 4, 3, 2, 1];
+  const output = [1, 2, 3, 4, 5];
+  expect(selectionSort(input)).toStrictEqual(output);
+});
+
+Deno.test('array with negative numbers', () => {
+  const input = [-3, 15, -7, 1, 12, -19, 5, 8, 2, -10];
+  const output = input.toSorted((a, b) => a - b);
+  expect(selectionSort(input)).toStrictEqual(output);
+});
+
+Deno.test('array with duplicates', () => {
+  const input = [5, 3, 8, 5, 2, 8, 1, 3];
+  const output = input.toSorted((a, b) => a - b);
   expect(selectionSort(input)).toStrictEqual(output);
 });
--- coding-interview-prep/algorithms/08-quick-sort.ts
+++ coding-interview-prep/algorithms/08-quick-sort.ts
@@ -7,11 +7,46 @@ function quickSort(nums: number[]): number[] {
 }
 
 // https://visualgo.net/en/sorting
-// https://www.w3schools.com/dsa/dsa_algo_insertionsort.php
+// https://www.w3schools.com/dsa/dsa_algo_quickSort.php
 
-Deno.test('test', () => {
+Deno.test('standard array', () => {
   const input = [3, 15, 7, 1, 12, 19, 5, 8, 2, 10];
   const output = input.toSorted((a, b) => a - b);
+  expect(quickSort(input)).toStrictEqual(output);
+});
 
+Deno.test('empty array', () => {
+  const input: number[] = [];
+  const output: number[] = [];
+  expect(quickSort(input)).toStrictEqual(output);
+});
+
+Deno.test('single element', () => {
+  const input = [42];
+  const output = [42];
+  expect(quickSort(input)).toStrictEqual(output);
+});
+
+Deno.test('already sorted array', () => {
+  const input = [1, 2, 3, 4, 5];
+  const output = [1, 2, 3, 4, 5];
+  expect(quickSort(input)).toStrictEqual(output);
+});
+
+Deno.test('reverse sorted array', () => {
+  const input = [5, 4, 3, 2, 1];
+  const output = [1, 2, 3, 4, 5];
+  expect(quickSort(input)).toStrictEqual(output);
+});
+
+Deno.test('array with negative numbers', () => {
+  const input = [-3, 15, -7, 1, 12, -19, 5, 8, 2, -10];
+  const output = input.toSorted((a, b) => a - b);
+  expect(quickSort(input)).toStrictEqual(output);
+});
+
+Deno.test('array with duplicates', () => {
+  const input = [5, 3, 8, 5, 2, 8, 1, 3];
+  const output = input.toSorted((a, b) => a - b);
   expect(quickSort(input)).toStrictEqual(output);
 });
