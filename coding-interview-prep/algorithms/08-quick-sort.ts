b34cfa0c340d1e01c40d678cea04cecb1dd805ba
Vien Dinh <maxvien@gmail.com>
2024-10-30 14:16:42 +0700

    ok

--- coding-interview-prep/algorithms/08-quick-sort.ts
+++ coding-interview-prep/algorithms/08-quick-sort.ts
@@ -3,22 +3,28 @@ import { expect } from 'jsr:@std/expect';
 function quickSort(arr: number[], left: number = 0, right: number = arr.length - 1): number[] {
   if (left < right) {
     const pivotIndex = partition(arr, left, right);
+
     quickSort(arr, left, pivotIndex - 1);
     quickSort(arr, pivotIndex + 1, right);
   }
+
   return arr;
 }
 
 function partition(arr: number[], left: number, right: number): number {
   const pivot = arr[right];
+
   let i = left - 1;
+
   for (let j = left; j < right; j++) {
     if (arr[j] <= pivot) {
       i++;
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
   }
+
   [arr[i + 1], arr[right]] = [arr[right], arr[i + 1]];
+
   return i + 1;
 }
 
