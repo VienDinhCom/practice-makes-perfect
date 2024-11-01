522f1e941fbdc82ec9df8421d3fcd1a7cfc4c736
Vien Dinh <maxvien@gmail.com>
2024-11-01 11:43:37 +0700

    take a break

--- back-end-development-and-apis/basic-node-and-express/myApp.js
+++ back-end-development-and-apis/basic-node-and-express/myApp.js
@@ -1,39 +1,30 @@
-let express = require('express');
-let app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+const express = require('express');
 
+dotenv.config();
 
+const app = express();
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
+// app.get('/json', (req, res) => {
+//   res.json({ message: 'Hello json' });
+// });
 
+app.get('/json', (req, res) => {
+  const message = 'Hello json';
+  const isUppercase = process.env.MESSAGE_STYLE === 'uppercase';
 
+  res.json({ message: isUppercase ? message.toUpperCase() : message });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
