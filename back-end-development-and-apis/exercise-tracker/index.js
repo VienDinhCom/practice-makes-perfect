03d941b6068ec3fdb05fed982f3127d0806d8df4
Vien Dinh <maxvien@gmail.com>
2024-11-05 11:30:33 +0700

    add user

--- back-end-development-and-apis/exercise-tracker/index.js
+++ back-end-development-and-apis/exercise-tracker/index.js
@@ -1,18 +1,72 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+dotenv.config();
+
+/* MongoDB
+ *********************************************************/
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+  },
 });
 
+const User = mongoose.model('User', userSchema);
 
+const exerciseSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  duration: {
+    type: Number,
+    require: true,
+  },
+  date: {
+    type: Date,
+    require: true,
+  },
+});
 
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
+/* Express
+ *********************************************************/
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/api/users', async (req, res) => {
+  const username = req.body.username.toLowerCase();
+
+  let user = await User.findOne({ username });
+
+  if (user) res.json(user);
+
+  user = await User.create({ username });
+
+  res.json(user);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
