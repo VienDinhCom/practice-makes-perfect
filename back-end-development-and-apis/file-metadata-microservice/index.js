1122ad79616f21cf4a14a7cfe1a0a05d4382027d
Vien Dinh <maxvien@gmail.com>
2024-11-06 16:57:49 +0700

    end: file-metadata-microservice

--- back-end-development-and-apis/file-metadata-microservice/index.js
+++ back-end-development-and-apis/file-metadata-microservice/index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { formidable } = require('formidable');
 
-var app = express();
+dotenv.config();
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', async (req, res) => {
+  const form = formidable();
 
+  const [_fields, files] = await form.parse(req);
 
+  const file = files.upfile[0];
+
+  res.json({
+    name: file.originalFilename,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
