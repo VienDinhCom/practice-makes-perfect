8cdf724625f988b39a65559bbe52c633311d1a64
Vien Dinh <maxvien@gmail.com>
2024-11-08 16:26:55 +0700

    relational-database/practice-postgresql-exercises/02-joins-and-subqueries/06-three-join-2.sql

--- relational-database/practice-postgresql-exercises/02-joins-and-subqueries/06-three-join-2.sql
+++ relational-database/practice-postgresql-exercises/02-joins-and-subqueries/06-three-join-2.sql
@@ -1,1 +1,20 @@
 -- https://pgexercises.com/questions/joins/threejoin2.html
+
+select
+  mems.firstname || ' ' || mems.surname as member,
+  facs.name as facility,
+  case
+    when mems.memid = 0 then facs.guestcost
+    else facs.membercost
+  end * bks.slots as cost
+from cd.bookings bks
+inner join cd.members mems using(memid)
+inner join cd.facilities facs using(facid)
+where
+  (case
+    when mems.memid = 0 then facs.guestcost
+    else facs.membercost
+  end * bks.slots) > 30
+  and bks.starttime >= '2012-09-14'
+  and bks.starttime < '2012-09-15'
+order by cost desc;
