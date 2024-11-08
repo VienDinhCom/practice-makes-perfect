917840f37b2c4d2cb6e9825db848feb76c8cb78f
Vien Dinh <maxvien@gmail.com>
2024-11-08 08:38:10 +0700

    end: review merge sort

--- data-structures-and-algorithms/algorithms/sorting/merge-sort.ts
+++ data-structures-and-algorithms/algorithms/sorting/merge-sort.ts
@@ -1,10 +1,29 @@
 import { expect } from 'jsr:@std/expect';
 
 function mergeSort(nums: number[]): number[] {
+  if (nums.length <= 1) return nums;
+
+  const midIndex = Math.floor(nums.length / 2);
+
+  const left = mergeSort(nums.slice(0, midIndex));
+  const right = mergeSort(nums.slice(midIndex));
+
   return merge(left, right);
 }
 
-function merge(left: number[], right: number[]): number[] {}
+function merge(left: number[], right: number[]): number[] {
+  const sortedNums: number[] = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedNums.push(left.shift()!);
+    } else {
+      sortedNums.push(right.shift()!);
+    }
+  }
+
+  return [...sortedNums, ...left, ...right];
+}
 
 // https://visualgo.net/en/sorting
 // https://www.youtube.com/watch?v=WprjBK0p6rw
