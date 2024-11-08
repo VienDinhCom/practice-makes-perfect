25742febe7fa3268872b3f645339b4b749c18c58
Vien Dinh <maxvien@gmail.com>
2024-11-08 14:50:19 +0700

    relational-database/practice-postgresql-exercises/02-joins-and-subqueries/02-simple-join-2.sql

--- relational-database/practice-postgresql-exercises/02-joins-and-subqueries/02-simple-join-2.sql
+++ relational-database/practice-postgresql-exercises/02-joins-and-subqueries/02-simple-join-2.sql
@@ -1,2 +1,12 @@
 -- https://pgexercises.com/questions/joins/simplejoin2.html
 
+select
+  starttime as start,
+  name
+from cd.bookings bks
+inner join cd.facilities facs using(facid)
+where
+  facs.name like 'Tennis Court%'
+  and bks.starttime >= '2012-09-21'
+  and bks.starttime < '2012-09-22'
+order by start;
