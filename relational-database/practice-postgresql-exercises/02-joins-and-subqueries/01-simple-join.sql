8de08a6d2be8547a6337063470b27d40d0f59c20
Vien Dinh <maxvien@gmail.com>
2024-11-08 14:34:08 +0700

    interesting

--- relational-database/practice-postgresql-exercises/02-joins-and-subqueries/01-simple-join.sql
+++ relational-database/practice-postgresql-exercises/02-joins-and-subqueries/01-simple-join.sql
@@ -3,3 +3,10 @@
 select starttime from cd.bookings bks
 inner join cd.members mems using(memid)
 where mems.firstname = 'David' and mems.surname = 'Farrell';
+
+-- OR
+
+select bks.starttime from cd.bookings bks, cd.members mems
+where mems.firstname='David'
+      and mems.surname='Farrell'
+      and mems.memid = bks.memid;
