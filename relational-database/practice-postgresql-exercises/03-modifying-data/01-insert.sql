4e7fb07a563be0ab50c470b3085d6761e272e288
Vien Dinh <maxvien@gmail.com>
2024-11-13 15:38:28 +0700

    relational-database/practice-postgresql-exercises/03-modifying-data/01-insert.sql

---
+++ relational-database/practice-postgresql-exercises/03-modifying-data/01-insert.sql
@@ -0,0 +1,3 @@
+-- https://pgexercises.com/questions/updates/insert.html
+
+insert into cd.facilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) values (9, 'Spa', 20, 30, 100000, 800);
