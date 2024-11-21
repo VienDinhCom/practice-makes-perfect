e3c3d585e11cf1f9b3030a52a05dc78c1bdbc1e1
Vien Dinh <maxvien@gmail.com>
2024-11-21 10:29:24 +0700

    implement: map class

---
+++ data-structures-and-algorithms/data-structures/map-class.ts
@@ -0,0 +1,129 @@
+import { expect } from 'jsr:@std/expect';
+
+class MapClass<T> {
+  private object: { [key: string]: T };
+
+  constructor() {
+    this.object = {};
+  }
+
+  add(key: string, value: T) {
+    if (this.has(key)) return false;
+
+    this.object[key] = value;
+
+    return true;
+  }
+
+  remove(key: string) {
+    delete this.object[key];
+  }
+
+  get(key: string) {
+    return this.object[key];
+  }
+
+  has(key: string) {
+    return this.object[key] !== undefined;
+  }
+
+  values() {
+    return Object.values(this.object);
+  }
+
+  entries() {
+    return Object.entries(this.object);
+  }
+
+  clear() {
+    Object.keys(this.object).forEach((key) => {
+      this.remove(key);
+    });
+  }
+
+  size() {
+    return Object.keys(this.object).length;
+  }
+}
+
+Deno.test('add and get methods', () => {
+  const map = new MapClass<number>();
+
+  // Add a new key-value pair
+  expect(map.add('a', 1)).toStrictEqual(true);
+  expect(map.get('a')).toStrictEqual(1);
+
+  // Attempt to add a duplicate key
+  expect(map.add('a', 2)).toStrictEqual(false);
+  expect(map.get('a')).toStrictEqual(1);
+});
+
+Deno.test('has method', () => {
+  const map = new MapClass<string>();
+
+  // Check for a key that does not exist
+  expect(map.has('a')).toStrictEqual(false);
+
+  // Add a key and check its existence
+  map.add('a', 'value');
+  expect(map.has('a')).toStrictEqual(true);
+});
+
+Deno.test('remove method', () => {
+  const map = new MapClass<number>();
+
+  map.add('a', 1);
+  expect(map.has('a')).toStrictEqual(true);
+
+  // Remove the key and verify
+  map.remove('a');
+  expect(map.has('a')).toStrictEqual(false);
+});
+
+Deno.test('values method', () => {
+  const map = new MapClass<number>();
+
+  map.add('a', 1);
+  map.add('b', 2);
+
+  expect(map.values()).toStrictEqual([1, 2]);
+});
+
+Deno.test('entries method', () => {
+  const map = new MapClass<number>();
+
+  map.add('a', 1);
+  map.add('b', 2);
+
+  expect(map.entries()).toStrictEqual([
+    ['a', 1],
+    ['b', 2],
+  ]);
+});
+
+Deno.test('clear method', () => {
+  const map = new MapClass<number>();
+
+  map.add('a', 1);
+  map.add('b', 2);
+
+  map.clear();
+
+  expect(map.size()).toStrictEqual(0);
+  expect(map.values()).toStrictEqual([]);
+});
+
+Deno.test('size method', () => {
+  const map = new MapClass<number>();
+
+  expect(map.size()).toStrictEqual(0);
+
+  map.add('a', 1);
+  map.add('b', 2);
+
+  expect(map.size()).toStrictEqual(2);
+
+  map.remove('a');
+
+  expect(map.size()).toStrictEqual(1);
+});
