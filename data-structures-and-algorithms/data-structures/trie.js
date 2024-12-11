76cb758c9c91e04ece0298b29a4d8f172d8d3c3a
Vien Dinh <maxvien@gmail.com>
2024-12-11 11:43:53 +0700

    learn: trie: add

---
+++ data-structures-and-algorithms/data-structures/trie.js
@@ -0,0 +1,30 @@
+const displayTree = (tree) => console.log(JSON.stringify(tree, null, 2));
+
+const Node = function () {
+  this.keys = new Map();
+  this.end = false;
+  this.setEnd = function () {
+    this.end = true;
+  };
+  this.isEnd = function () {
+    return this.end;
+  };
+};
+
+const Trie = function () {
+  this.root = new Node();
+
+  this.add = (word) => {
+    let current = this.root;
+
+    for (const char of word) {
+      if (!current.keys.has(char)) {
+        current.keys.set(char, new Node());
+      }
+
+      current = current.keys.get(char);
+    }
+
+    current.end = true;
+  };
+};
