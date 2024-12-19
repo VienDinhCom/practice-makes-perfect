980c81f0addf292e48cf9da2fad2b006da46699c
Vien Dinh <maxvien@gmail.com>
2024-12-19 15:07:48 +0700

    postgresql-exercises/04-aggregation/20-classify.sql

--- postgresql-exercises/04-aggregation/20-classify.sql
+++ postgresql-exercises/04-aggregation/20-classify.sql
@@ -1,2 +1,20 @@
 -- https://pgexercises.com/questions/aggregates/classify.html
 
+with subquery as (
+  select
+    facs.name,
+    ntile(3) over(order by sum(case when memid=0 then facs.guestcost else facs.membercost end * slots) desc) as class
+  from cd.bookings bks
+  inner join cd.facilities facs using(facid)
+  group by facs.facid, bks.facid
+)
+
+select
+  name,
+  case
+    when class=1 then 'high'
+    when class=2 then 'average'
+    else 'low'
+  end as revenue
+from subquery
+order by class, name;
