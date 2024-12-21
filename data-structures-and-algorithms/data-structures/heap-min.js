c04927f8bfea5e76b4b53ab4e2572f97a0dce6a3
Vien Dinh <maxvien@gmail.com>
2024-12-21 08:49:16 +0700

    learn: min heap: insert

--- data-structures-and-algorithms/data-structures/heap-min.js
+++ data-structures-and-algorithms/data-structures/heap-min.js
@@ -1,6 +1,6 @@
 class MinHeap {
   constructor() {
-    this.heap = [];
+    this.heap = [null];
   }
 
   static isSorted(a) {
@@ -18,5 +18,31 @@ class MinHeap {
       a[i] = Math.floor(Math.random() * 100);
     }
     return a;
+  }
+
+  leftChildIndex(index) {
+    return 2 * index;
+  }
+
+  rightChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  parentIndex(index) {
+    return Math.floor(index / 2);
+  }
+
+  insert(item) {
+    this.heap.push(item);
+
+    let current = this.heap.length - 1;
+    let parent = this.parentIndex(current);
+
+    while (current > 1 && this.heap[current] < this.heap[parent]) {
+      [this.heap[current], this.heap[parent]] = [this.heap[parent], this.heap[current]];
+
+      current = parent;
+      parent = this.parentIndex(current);
+    }
   }
 }
