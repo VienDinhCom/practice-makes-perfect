fee221fbe649b4992a77add998d9b091cb5775d8
Vien Dinh <maxvien@gmail.com>
2024-12-21 14:46:17 +0700

    react: render-state-in-the-user-interface

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,18 +1,18 @@
-class StatefulComponent extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
-    // Only change code below this line
-
     this.state = {
-      firstName: 'Vien',
+      name: 'freeCodeCamp',
     };
-
-    // Only change code above this line
   }
   render() {
     return (
       <div>
-        <h1>{this.state.firstName}</h1>
+        {/* Change code below this line */}
+
+        <h1>{this.state.name}</h1>
+
+        {/* Change code above this line */}
       </div>
     );
   }
