37eb0c41385884cd131dc3f2ef0360f4705f11a1
Vien Dinh <maxvien@gmail.com>
2024-12-21 14:47:27 +0700

    react: render-state-in-the-user-interface-another-way

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -6,11 +6,15 @@ class MyComponent extends React.Component {
     };
   }
   render() {
+    // Change code below this line
+
+    const { name } = this.state;
+
+    // Change code above this line
     return (
       <div>
         {/* Change code below this line */}
-
-        <h1>{this.state.name}</h1>
+        <h1>{name}</h1>
 
         {/* Change code above this line */}
       </div>
