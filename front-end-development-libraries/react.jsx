1430170159bc7e90e73f6e2e77fe8f13ffd2307f
Vien Dinh <maxvien@gmail.com>
2024-12-23 16:49:37 +0700

    react: use-the-lifecycle-method-componentdidmount

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,15 +1,24 @@
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      activeUsers: null,
+    };
   }
-  componentWillMount() {
-    // Change code below this line
-
-    console.log('will mount');
-
-    // Change code above this line
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273,
+      });
+    }, 2500);
   }
   render() {
-    return <div />;
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
   }
 }
