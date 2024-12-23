9f0a07279ed30d79f74f9240a397084604c6ef7a
Vien Dinh <maxvien@gmail.com>
2024-12-23 17:10:47 +0700

    react: add-inline-styles-in-react

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,5 +1,14 @@
+const styles = {
+  color: 'purple',
+  fontSize: 40,
+  border: '2px solid purple',
+};
+
+// Change code above this line
 class Colorful extends React.Component {
   render() {
-    return <div style={{ color: 'red', fontSize: '72px' }}>Big Red</div>;
+    // Change code below this line
+    return <div style={styles}>Style Me!</div>;
+    // Change code above this line
   }
 }
