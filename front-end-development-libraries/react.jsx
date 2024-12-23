bef6e2cea56266f6356253e034fd971b2fd8e047
Vien Dinh <maxvien@gmail.com>
2024-12-23 16:59:00 +0700

    react: add-event-listeners

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -2,22 +2,34 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeUsers: null,
+      message: '',
     };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
+  // Change code below this line
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        activeUsers: 1273,
-      });
-    }, 2500);
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+  // Change code above this line
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! ',
+    }));
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
   }
   render() {
     return (
       <div>
-        {/* Change code below this line */}
-        <h1>Active Users: {this.state.activeUsers}</h1>
-        {/* Change code above this line */}
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
