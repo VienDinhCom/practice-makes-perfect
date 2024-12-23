befe08163ac0c52b74759d0a8d13ded9caa6cf8b
Vien Dinh <maxvien@gmail.com>
2024-12-23 15:35:59 +0700

    react: write-a-simple-counter

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,42 +1,62 @@
-class MyComponent extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visibility: false,
+      count: 0,
     };
     // Change code below this line
 
-    this.toggleVisibility = this.toggleVisibility.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
 
     // Change code above this line
   }
   // Change code below this line
-
-  toggleVisibility() {
+  increment() {
     this.setState((state) => {
       const draft = structuredClone(state);
 
-      draft.visibility = !draft.visibility;
+      draft.count++;
 
       return draft;
     });
   }
 
+  decrement() {
+    this.setState((state) => {
+      const draft = structuredClone(state);
+
+      draft.count--;
+
+      return draft;
+    });
+  }
+
+  reset() {
+    this.setState((state) => {
+      const draft = structuredClone(state);
+
+      draft.count = 0;
+
+      return draft;
+    });
+  }
   // Change code above this line
   render() {
-    if (this.state.visibility) {
-      return (
-        <div>
-          <button onClick={this.toggleVisibility}>Click Me</button>
-          <h1>Now you see me!</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button onClick={this.toggleVisibility}>Click Me</button>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <button className="inc" onClick={this.increment}>
+          Increment!
+        </button>
+        <button className="dec" onClick={this.decrement}>
+          Decrement!
+        </button>
+        <button className="reset" onClick={this.reset}>
+          Reset
+        </button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
   }
 }
