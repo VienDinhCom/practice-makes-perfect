a9544710695bb2c7bc29938862950394e39410b9
Vien Dinh <maxvien@gmail.com>
2024-12-23 16:46:48 +0700

    react: pass-a-callback-as-props

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -2,30 +2,50 @@ class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'CamperBot',
+      inputValue: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value,
+    });
   }
   render() {
     return (
       <div>
         {/* Change code below this line */}
-        <Navbar name={this.state.name} />
+        <GetInput handleChange={this.handleChange} />
+        <RenderInput input={this.state.inputValue} />
         {/* Change code above this line */}
       </div>
     );
   }
 }
 
-class Navbar extends React.Component {
+class GetInput extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        {/* Change code below this line */}
-        <h1>Hello, my name is: {this.props.name}</h1>
-        {/* Change code above this line */}
+        <h3>Get Input:</h3>
+        <input value={this.props.input} onChange={this.props.handleChange} />
+      </div>
+    );
+  }
+}
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
       </div>
     );
   }
