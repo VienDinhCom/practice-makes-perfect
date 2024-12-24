980dd8ee1a40bc8eb024a5ca5f9c57ef5b75e7d8
Vien Dinh <maxvien@gmail.com>
2024-12-24 16:08:42 +0700

    react: render-conditionally-from-props

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,48 +1,45 @@
-const inputStyle = {
-  width: 235,
-  margin: 5,
-};
-
-class CheckUserAge extends React.Component {
+class Results extends React.Component {
   constructor(props) {
     super(props);
-    // Change code below this line
-
-    this.state = {
-      input: '',
-      userAge: '',
-    };
-
-    // Change code above this line
-    this.submit = this.submit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    this.setState({
-      input: e.target.value,
-      userAge: '',
-    });
-  }
-  submit() {
-    this.setState((state) => ({
-      userAge: state.input,
-    }));
   }
   render() {
-    const buttonOne = <button onClick={this.submit}>Submit</button>;
-    const buttonTwo = <button>You May Enter</button>;
-    const buttonThree = <button>You Shall Not Pass</button>;
+    {
+      /* Change code below this line */
+    }
+    return <h1>{this.props.fiftyFifty ? 'You Win!' : 'You Lose!'}</h1>;
+    {
+      /* Change code above this line */
+    }
+  }
+}
 
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+      fiftyFifty: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState) => {
+      // Complete the return statement:
+      return {
+        counter: prevState.counter + 1,
+        fiftyFifty: Math.random() >= 0.5,
+      };
+    });
+  }
+  render() {
+    const expression = null; // Change this line
     return (
       <div>
-        <h3>Enter Your Age to Continue</h3>
-        <input style={inputStyle} type="number" value={this.state.input} onChange={this.handleChange} />
-        <br />
+        <button onClick={this.handleClick}>Play Again</button>
         {/* Change code below this line */}
-
-        {this.state.userAge === '' ? buttonOne : this.state.userAge < 18 ? buttonThree : buttonTwo}
-
+        <Results fiftyFifty={this.state.fiftyFifty} />
         {/* Change code above this line */}
+        <p>{'Turn: ' + this.state.counter}</p>
       </div>
     );
   }
