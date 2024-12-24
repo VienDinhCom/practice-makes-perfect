c8ab4d12e56d3a8eba6233dc882bae96e218d57c
Vien Dinh <maxvien@gmail.com>
2024-12-24 15:34:09 +0700

    react: render-with-an-if-else-condition

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,68 +1,32 @@
-const inputStyle = {
-  width: 235,
-  margin: 5,
-};
-
-class MagicEightBall extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userInput: '',
-      randomIndex: '',
+      display: true,
     };
-    this.ask = this.ask.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.toggleDisplay = this.toggleDisplay.bind(this);
   }
-  ask() {
-    if (this.state.userInput) {
-      this.setState({
-        randomIndex: Math.floor(Math.random() * 20),
-        userInput: '',
-      });
-    }
-  }
-  handleChange(event) {
-    this.setState({
-      userInput: event.target.value,
-    });
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
   }
   render() {
-    const possibleAnswers = [
-      'It is certain',
-      'It is decidedly so',
-      'Without a doubt',
-      'Yes, definitely',
-      'You may rely on it',
-      'As I see it, yes',
-      'Outlook good',
-      'Yes',
-      'Signs point to yes',
-      'Reply hazy try again',
-      'Ask again later',
-      'Better not tell you now',
-      'Cannot predict now',
-      'Concentrate and ask again',
-      "Don't count on it",
-      'My reply is no',
-      'My sources say no',
-      'Most likely',
-      'Outlook not so good',
-      'Very doubtful',
-    ];
-    const answer = possibleAnswers[this.state.randomIndex]; // Change this line
-    return (
-      <div>
-        <input type="text" value={this.state.userInput} onChange={this.handleChange} style={inputStyle} />
-        <br />
-        <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
-        <br />
-        <h3>Answer:</h3>
-        <p>
-          {/* Change code below this line */}
-          {answer}
-          {/* Change code above this line */}
-        </p>
-      </div>
-    );
+    // Change code below this line
+
+    if (this.state.display) {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
   }
 }
