8736b0e69ee2bafa0fcd2ceac51d7b3e9cca980e
Vien Dinh <maxvien@gmail.com>
2024-12-24 15:37:50 +0700

    react: use--for-a-more-concise-conditional

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -6,27 +6,21 @@ class MyComponent extends React.Component {
     };
     this.toggleDisplay = this.toggleDisplay.bind(this);
   }
+
   toggleDisplay() {
     this.setState((state) => ({
       display: !state.display,
     }));
   }
+
   render() {
     // Change code below this line
 
-    if (this.state.display) {
-      return (
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-          <h1>Displayed!</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+      </div>
+    );
   }
 }
