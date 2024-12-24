bd789ae01c821ab518319798d66ec2b2c903c454
Vien Dinh <maxvien@gmail.com>
2024-12-24 15:25:16 +0700

    react: use-advanced-javascript-in-react-render-method

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,14 +1,68 @@
-const styles = {
-  color: 'purple',
-  fontSize: 40,
-  border: '2px solid purple',
+const inputStyle = {
+  width: 235,
+  margin: 5,
 };
 
-// Change code above this line
-class Colorful extends React.Component {
+class MagicEightBall extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+      randomIndex: '',
+    };
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  ask() {
+    if (this.state.userInput) {
+      this.setState({
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: '',
+      });
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      userInput: event.target.value,
+    });
+  }
   render() {
-    // Change code below this line
-    return <div style={styles}>Style Me!</div>;
-    // Change code above this line
+    const possibleAnswers = [
+      'It is certain',
+      'It is decidedly so',
+      'Without a doubt',
+      'Yes, definitely',
+      'You may rely on it',
+      'As I see it, yes',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and ask again',
+      "Don't count on it",
+      'My reply is no',
+      'My sources say no',
+      'Most likely',
+      'Outlook not so good',
+      'Very doubtful',
+    ];
+    const answer = possibleAnswers[this.state.randomIndex]; // Change this line
+    return (
+      <div>
+        <input type="text" value={this.state.userInput} onChange={this.handleChange} style={inputStyle} />
+        <br />
+        <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+        <br />
+        <h3>Answer:</h3>
+        <p>
+          {/* Change code below this line */}
+          {answer}
+          {/* Change code above this line */}
+        </p>
+      </div>
+    );
   }
 }
