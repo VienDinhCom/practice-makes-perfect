b8779904d8fd2d61ad6dcbec49ef010bf51c5f92
Vien Dinh <maxvien@gmail.com>
2024-12-24 15:56:34 +0700

    react: use-a-ternary-expression-for-conditional-rendering

--- front-end-development-libraries/react.jsx
+++ front-end-development-libraries/react.jsx
@@ -1,25 +1,48 @@
-class MyComponent extends React.Component {
+const inputStyle = {
+  width: 235,
+  margin: 5,
+};
+
+class CheckUserAge extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      display: true,
-    };
-    this.toggleDisplay = this.toggleDisplay.bind(this);
-  }
+    // Change code below this line
 
-  toggleDisplay() {
+    this.state = {
+      input: '',
+      userAge: '',
+    };
+
+    // Change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: '',
+    });
+  }
+  submit() {
     this.setState((state) => ({
-      display: !state.display,
+      userAge: state.input,
     }));
   }
-
   render() {
-    // Change code below this line
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
 
     return (
       <div>
-        <button onClick={this.toggleDisplay}>Toggle Display</button>
-        {this.state.display && <h1>Displayed!</h1>}
+        <h3>Enter Your Age to Continue</h3>
+        <input style={inputStyle} type="number" value={this.state.input} onChange={this.handleChange} />
+        <br />
+        {/* Change code below this line */}
+
+        {this.state.userAge === '' ? buttonOne : this.state.userAge < 18 ? buttonThree : buttonTwo}
+
+        {/* Change code above this line */}
       </div>
     );
   }
