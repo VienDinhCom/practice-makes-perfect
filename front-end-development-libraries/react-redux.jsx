0fec8d02c714c2dccc436cb68074bc7b88a7d597
Vien Dinh <maxvien@gmail.com>
2024-12-25 16:51:14 +0700

    react-and-redux/manage-state-locally-first

--- front-end-development-libraries/react-redux.jsx
+++ front-end-development-libraries/react-redux.jsx
@@ -1,16 +1,45 @@
+import React from 'react';
+
 class DisplayMessages extends React.Component {
-  // Change code below this line
   constructor(props) {
     super(props);
-
     this.state = {
       input: '',
       messages: [],
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
   }
 
-  // Change code above this line
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
+  submitMessage() {
+    if (this.state.input.trim()) {
+      // Check if input is not empty
+      this.setState((prevState) => ({
+        messages: [...prevState.messages, prevState.input],
+        input: '', // Clear the input after submission
+      }));
+    }
+  }
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input type="text" value={this.state.input} onChange={this.handleChange} />
+        <button onClick={this.submitMessage}>Add Message</button>
+        <ul>
+          {this.state.messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
+
+export default DisplayMessages;
