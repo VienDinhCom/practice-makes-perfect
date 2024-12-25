fbf8158edd4e0d037fdcbfa8f0cc0892bacd2c6c
Vien Dinh <maxvien@gmail.com>
2024-12-25 17:34:31 +0700

    react-and-redux/connect-redux-to-react

--- front-end-development-libraries/react-redux.jsx
+++ front-end-development-libraries/react-redux.jsx
@@ -5,10 +5,30 @@ const addMessage = (message) => {
   };
 };
 
-// Change code below this line
+const mapStateToProps = (state) => {
+  return {
+    messages: state,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    submitNewMessage: (message) => dispatch(addMessage(message)),
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message));
+    },
   };
 };
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>;
+  }
+}
+
+const connect = ReactRedux.connect;
+// Change code below this line
+
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational);
