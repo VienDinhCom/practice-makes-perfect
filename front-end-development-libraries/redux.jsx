3244bc741f22d54eca4e3c895350247015a21e8c
Vien Dinh <maxvien@gmail.com>
2024-12-25 15:07:46 +0700

    react: handle-an-action-in-the-store

--- front-end-development-libraries/redux.jsx
+++ front-end-development-libraries/redux.jsx
@@ -1,11 +1,30 @@
-const store = Redux.createStore((state = { login: false }) => state);
+const defaultState = {
+  login: false,
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+
+  const draft = structuredClone(state);
+
+  switch (action.type) {
+    case 'LOGIN':
+      draft.login = true;
+      break;
+
+    default:
+      break;
+  }
+
+  return draft;
+
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
 
 const loginAction = () => {
   return {
     type: 'LOGIN',
   };
 };
-
-// Dispatch the action here:
-
-store.dispatch(loginAction());
