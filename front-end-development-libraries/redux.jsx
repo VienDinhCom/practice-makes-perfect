106f30f23cb3195f3af092173b8a13b951aa59c4
Vien Dinh <maxvien@gmail.com>
2024-12-25 15:46:47 +0700

    redux/use-the-spread-operator-on-arrays

--- front-end-development-libraries/redux.jsx
+++ front-end-development-libraries/redux.jsx
@@ -1,13 +1,8 @@
-const ADD_TO_DO = 'ADD_TO_DO';
-
-// A list of strings representing tasks to do:
-const todos = ['Go to the store', 'Clean the house', 'Cook dinner', 'Learn to code'];
-
-const immutableReducer = (state = todos, action) => {
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
   switch (action.type) {
-    case ADD_TO_DO:
+    case 'ADD_TO_DO':
       // Don't mutate state here or the tests will fail
-      return [...todos, action.todo];
+      return [...state, action.todo];
     default:
       return state;
   }
@@ -15,7 +10,7 @@ const immutableReducer = (state = todos, action) => {
 
 const addToDo = (todo) => {
   return {
-    type: ADD_TO_DO,
+    type: 'ADD_TO_DO',
     todo,
   };
 };
