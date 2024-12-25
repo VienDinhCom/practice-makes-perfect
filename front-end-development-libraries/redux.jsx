f11f77f4fb654b446eb44349ff23fb32b9aa2842
Vien Dinh <maxvien@gmail.com>
2024-12-25 15:41:52 +0700

    react: write-a-counter-with-redux

--- front-end-development-libraries/redux.jsx
+++ front-end-development-libraries/redux.jsx
@@ -1,50 +1,33 @@
-const REQUESTING_DATA = 'REQUESTING_DATA';
-const RECEIVED_DATA = 'RECEIVED_DATA';
+const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // Define a constant for decrement action types
 
-const requestingData = () => {
-  return { type: REQUESTING_DATA };
-};
-const receivedData = (data) => {
-  return { type: RECEIVED_DATA, users: data.users };
-};
-
-const handleAsync = () => {
-  return function (dispatch) {
-    // Dispatch request action here
-
-    dispatch(requestingData());
-
-    setTimeout(function () {
-      let data = {
-        users: ['Jeff', 'William', 'Alice'],
-      };
-
-      dispatch(receivedData(data));
-      // Dispatch received data action here
-    }, 2500);
-  };
-};
-
-const defaultState = {
-  fetching: false,
-  users: [],
-};
-
-const asyncDataReducer = (state = defaultState, action) => {
+// Define the counter reducer which will increment or decrement the state based on the action it receives
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
-    case REQUESTING_DATA:
-      return {
-        fetching: true,
-        users: [],
-      };
-    case RECEIVED_DATA:
-      return {
-        fetching: false,
-        users: action.users,
-      };
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
     default:
       return state;
   }
 };
 
-const store = Redux.createStore(asyncDataReducer, Redux.applyMiddleware(ReduxThunk.default));
+// Define an action creator for incrementing
+const incAction = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+// Define an action creator for decrementing
+const decAction = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+// Define the Redux store here, passing in your reducers
+const store = Redux.createStore(counterReducer);
