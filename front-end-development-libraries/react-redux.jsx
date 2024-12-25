b6f06d3250dec978b154110834dd439e0ef20b37
Vien Dinh <maxvien@gmail.com>
2024-12-25 16:58:23 +0700

    react-and-redux/extract-state-logic-to-redux

--- front-end-development-libraries/react-redux.jsx
+++ front-end-development-libraries/react-redux.jsx
@@ -43,3 +43,21 @@ class DisplayMessages extends React.Component {
 }
 
 export default DisplayMessages;
+
+const ADD = 'ADD';
+
+function messageReducer(state = [], action) {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.message];
+
+    default:
+      return state;
+  }
+}
+
+function addMessage(message) {
+  return { type: ADD, message };
+}
+
+const store = Redux.createStore(messageReducer);
