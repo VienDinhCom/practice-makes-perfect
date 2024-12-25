1ce7fa30607f998fb03e16b5c0cf2462b5494eb7
Vien Dinh <maxvien@gmail.com>
2024-12-25 16:35:45 +0700

    react-and-redux/getting-started-with-react-redux

---
+++ front-end-development-libraries/react-redux.jsx
@@ -0,0 +1,16 @@
+class DisplayMessages extends React.Component {
+  // Change code below this line
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      messages: [],
+    };
+  }
+
+  // Change code above this line
+  render() {
+    return <div />;
+  }
+}
