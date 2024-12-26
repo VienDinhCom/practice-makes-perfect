9a946670929c7915c6bdb8ece180127004ffcb1d
Vien Dinh <maxvien@gmail.com>
2024-12-26 17:47:15 +0700

    frontend: random-quote-machine: fix build

--- front-end-development-libraries/vite.config.ts
+++ front-end-development-libraries/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'random-quote-machine.html'),
+        // Add more entry points as needed
+      },
+    },
+  },
   plugins: [react()],
-})
+});
