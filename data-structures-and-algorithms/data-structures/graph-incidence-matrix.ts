520fb50bb9d73791c5d9390e086a588297cc1468
Vien Dinh <maxvien@gmail.com>
2024-12-27 12:25:52 +0700

    graph: incidence matrix

---
+++ data-structures-and-algorithms/data-structures/graph-incidence-matrix.ts
@@ -0,0 +1,9 @@
+// https://www.perplexity.ai/search/use-simple-terms-to-explain-th-XgicU0NST0abT3sHBXfcYQ
+
+const incMatUndirected = [
+  [1, 0, 0, 0],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0],
+  [0, 0, 0, 1],
+  [0, 0, 1, 0],
+];
