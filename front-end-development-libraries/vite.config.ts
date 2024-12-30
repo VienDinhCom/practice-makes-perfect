d8ce6c5cb52e23f6588c4eb8e13d2c702346dc6d
Vien Dinh <maxvien@gmail.com>
2024-12-30 12:11:06 +0700

    up

--- front-end-development-libraries/vite.config.ts
+++ front-end-development-libraries/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         quote: resolve(__dirname, 'random-quote-machine.html'),
         markdown: resolve(__dirname, 'markdown-previewer.html'),
+        drumMachine: resolve(__dirname, 'drum-machine.html'),
       },
     },
   },
