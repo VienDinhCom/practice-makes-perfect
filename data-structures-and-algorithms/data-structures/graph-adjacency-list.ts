99ee686409b03c404cb638f49b6eb2c9b5a77e73
Vien Dinh <maxvien@gmail.com>
2024-12-31 09:00:41 +0700

    review: graph: graph-adjacency-list

--- data-structures-and-algorithms/data-structures/graph-adjacency-list.ts
+++ data-structures-and-algorithms/data-structures/graph-adjacency-list.ts
@@ -1,6 +1,8 @@
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/adjacency-list
+
 const undirectedAdjList = {
+  Jeff: ['James', 'Jenny'],
   James: ['Jeff'],
   Jill: ['Jenny'],
   Jenny: ['Jill', 'Jeff'],
-  Jeff: ['James', 'Jenny'],
 };
