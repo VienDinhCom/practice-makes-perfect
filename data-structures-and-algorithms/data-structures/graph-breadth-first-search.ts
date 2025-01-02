8caeefaeab1b2d04d82b23d56ae602ad858e3fd6
Vien Dinh <maxvien@gmail.com>
2025-01-02 11:12:57 +0700

    graph: breadth-first-search: convert to ts and add tests

---
+++ data-structures-and-algorithms/data-structures/graph-breadth-first-search.ts
@@ -0,0 +1,77 @@
+import { expect } from 'jsr:@std/expect';
+
+function breadthFirstSearch(graph: number[][], root: number) {
+  const distances: Record<number, number> = {};
+  const queue: number[] = [];
+
+  graph.forEach((_, index) => {
+    distances[index] = Infinity;
+  });
+
+  distances[root] = 0;
+  queue.push(root);
+
+  while (queue.length) {
+    const node = queue.shift()!;
+
+    graph[node].forEach((connected, neighbor) => {
+      if (connected && distances[neighbor] === Infinity) {
+        distances[neighbor] = distances[node] + 1;
+
+        queue.push(neighbor);
+      }
+    });
+  }
+
+  return distances;
+}
+
+Deno.test('Test 1', () => {
+  const adjacencyMatrixGraph = [
+    [0, 1, 0, 0],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [0, 0, 1, 0],
+  ];
+
+  const distances = breadthFirstSearch(adjacencyMatrixGraph, 1);
+
+  expect(distances).toStrictEqual({ 0: 1, 1: 0, 2: 1, 3: 2 });
+});
+
+Deno.test('Test 1', () => {
+  const adjacencyMatrixGraph = [
+    [0, 1, 0, 0],
+    [1, 0, 1, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  const distances = breadthFirstSearch(adjacencyMatrixGraph, 1);
+
+  expect(distances).toStrictEqual({ 0: 1, 1: 0, 2: 1, 3: Infinity });
+});
+
+Deno.test('Test 3', () => {
+  const adjacencyMatrixGraph = [
+    [0, 1, 0, 0],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [0, 0, 1, 0],
+  ];
+
+  const distances = breadthFirstSearch(adjacencyMatrixGraph, 0);
+
+  expect(distances).toStrictEqual({ 0: 0, 1: 1, 2: 2, 3: 3 });
+});
+
+Deno.test('Test 4', () => {
+  const adjacencyMatrixGraph = [
+    [0, 1],
+    [1, 0],
+  ];
+
+  const distances = breadthFirstSearch(adjacencyMatrixGraph, 0);
+
+  expect(distances).toStrictEqual({ 0: 0, 1: 1 });
+});
