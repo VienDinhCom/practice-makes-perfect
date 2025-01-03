fb0a28090da0683f09917751028f86f9fb2d2fe6
Vien Dinh <maxvien@gmail.com>
2025-01-03 11:29:14 +0700

    rename: graph: learn

---
+++ data-structures-and-algorithms/data-structures/graph-learn-adjacency-matrix.ts
@@ -0,0 +1,18 @@
+// https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/adjacency-matrix
+
+// review
+const adjMatUndirected = [
+  [0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [0, 0, 1, 1, 0],
+];
+
+// const adjMatUndirected = [
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0],
+// ];
