a97fdd5df40795b0acb351182497f5c5af19bab5
Vien Dinh <maxvien@gmail.com>
2025-01-07 16:07:30 +0700

    test: javascript: template

--- javascript-algorithms-and-data-structures/roman-numeral-converter.ts
+++ javascript-algorithms-and-data-structures/roman-numeral-converter.ts
@@ -1,1 +1,9 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
+
+import { expect } from 'jsr:@std/expect';
+
+function convertToRoman(num: number): string {}
+
+Deno.test('...', () => {
+  expect(...).toStrictEqual(...);
+});
--- javascript-algorithms-and-data-structures/telephone-number-validator.ts
+++ javascript-algorithms-and-data-structures/telephone-number-validator.ts
@@ -1,1 +1,9 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/telephone-number-validator
+
+import { expect } from 'jsr:@std/expect';
+
+
+Deno.test('...', () => {
+  expect(...).toStrictEqual(...);
+});
+
