d69abc9e9ced214d46319012086c661a3157d070
Vien Dinh <maxvien@gmail.com>
2025-01-07 16:07:30 +0700

    test: javascript: template

--- javascript-algorithms-and-data-structures/cash-register.ts
+++ javascript-algorithms-and-data-structures/cash-register.ts
@@ -1,1 +1,10 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
+
+
+import { expect } from 'jsr:@std/expect';
+
+
+Deno.test('...', () => {
+  expect(...).toStrictEqual(...);
+});
+
