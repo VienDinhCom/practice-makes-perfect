e026412e5a321a47679fc39692462a1e94ea4b4a
Vien Dinh <maxvien@gmail.com>
2025-01-24 12:13:35 +0700

    review: dsa: bst: findMinHeight

--- freecodecamp-coding-interview-prep/data-structures/binary-search-tree.ts
+++ freecodecamp-coding-interview-prep/data-structures/binary-search-tree.ts
@@ -123,33 +123,35 @@ class BinarySearchTree {
     return traverse(this.root);
   }
 
-  // findMinHeight() {
-  //   const heights: number[] = [];
+  findMinHeight(): number {
+    const heights: number[] = [];
 
-  //   const traverse = (node: Node | null, height: number) => {
-  //     if (node === null) {
-  //       heights.push(height - 1);
-  //       return;
-  //     }
+    const traverse = (node: Node | null, height: number) => {
+      if (node === null) {
+        heights.push(height);
 
-  //     traverse(node!.left, height + 1);
-  //     traverse(node!.right, height + 1);
-  //   };
+        return;
+      }
 
-  //   traverse(this.root, 0);
+      traverse(node.left, height + 1);
 
-  //   return Math.min(...heights);
-  // }
-
-  findMinHeight() {
-    const traverse = (node: Node | null): number => {
-      if (node === null) return -1;
-
-      return 1 + Math.min(traverse(node.left), traverse(node.right));
+      traverse(node.right, height + 1);
     };
 
-    return traverse(this.root);
+    traverse(this.root, -1);
+
+    return Math.min(...heights);
   }
+
+  // findMinHeight() {
+  //   const traverse = (node: Node | null): number => {
+  //     if (node === null) return -1;
+
+  //     return 1 + Math.min(traverse(node.left), traverse(node.right));
+  //   };
+
+  //   return traverse(this.root);
+  // }
 
   isBalanced() {
     return this.findMaxHeight() === this.findMinHeight();
