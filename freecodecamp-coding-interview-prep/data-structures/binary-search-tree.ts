cb317216d7a025358ca8af3767e75f978a5a997b
Vien Dinh <maxvien@gmail.com>
2025-01-24 11:39:41 +0700

    review: dsa: bst: findMaxHeight 1

--- freecodecamp-coding-interview-prep/data-structures/binary-search-tree.ts
+++ freecodecamp-coding-interview-prep/data-structures/binary-search-tree.ts
@@ -94,33 +94,34 @@ class BinarySearchTree {
   }
 
   // https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-find-the-minimum-and-maximum-height-of-a-binary-search-tree/301641
-  // findMaxHeight() {
-  //   const heights: number[] = [];
-
-  //   const traverse = (node: Node | null, height: number) => {
-  //     if (node === null) {
-  //       heights.push(height - 1);
-  //       return;
-  //     }
-
-  //     traverse(node!.left, height + 1);
-  //     traverse(node!.right, height + 1);
-  //   };
-
-  //   traverse(this.root, 0);
-
-  //   return Math.max(...heights);
-  // }
-
   findMaxHeight() {
-    const traverse = (node: Node | null): number => {
-      if (node === null) return -1;
+    const heights: number[] = [];
 
-      return 1 + Math.max(traverse(node.left), traverse(node.right));
+    const traverse = (node: Node | null, height: number) => {
+      if (node === null) {
+        heights.push(height);
+
+        return;
+      }
+
+      traverse(node.left, height + 1);
+      traverse(node.right, height + 1);
     };
 
-    return traverse(this.root);
+    traverse(this.root, -1);
+
+    return Math.max(...heights);
   }
+
+  // findMaxHeight() {
+  //   const traverse = (node: Node | null): number => {
+  //     if (node === null) return -1;
+
+  //     return 1 + Math.max(traverse(node.left), traverse(node.right));
+  //   };
+
+  //   return traverse(this.root);
+  // }
 
   // findMinHeight() {
   //   const heights: number[] = [];
