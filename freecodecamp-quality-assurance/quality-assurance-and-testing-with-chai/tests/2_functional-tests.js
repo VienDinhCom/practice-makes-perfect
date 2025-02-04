f9d80e18733469140e093085c37435fe19304a7a
Vien Dinh <maxvien@gmail.com>
2025-02-04 17:16:15 +0700

    quality-assurance-and-testing-with-chai: Functional Tests

--- freecodecamp-quality-assurance/quality-assurance-and-testing-with-chai/tests/2_functional-tests.js
+++ freecodecamp-quality-assurance/quality-assurance-and-testing-with-chai/tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -28,8 +28,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
@@ -39,18 +39,31 @@ suite('Functional Tests', function () {
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        .send({ surname: 'Colombo' })
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
 
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+      .request(server)
+      .keepOpen()
+      .put('/travellers')
+      .send({ surname: 'da Verrazzano' })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.name, 'Giovanni');
+        assert.equal(res.body.surname, 'da Verrazzano');
 
-      done();
+        done();
+      });
     });
   });
 });
@@ -60,10 +73,8 @@ const Browser = require('zombie');
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
-
-
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
+    test('should have a working "site" property', function () {
       assert.isNotNull(browser.site);
     });
   });
