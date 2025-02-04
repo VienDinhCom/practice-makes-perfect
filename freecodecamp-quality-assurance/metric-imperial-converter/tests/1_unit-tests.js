a7221e501dd9348131e16678d0e8f89d4a238060
Vien Dinh <maxvien@gmail.com>
2025-02-04 15:39:29 +0700

    init: quality-assurance-and-testing-with-chai

--- freecodecamp-quality-assurance/metric-imperial-converter/tests/1_unit-tests.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
-
-let convertHandler = new ConvertHandler();
-
-suite('Unit Tests', function(){
-
-});
