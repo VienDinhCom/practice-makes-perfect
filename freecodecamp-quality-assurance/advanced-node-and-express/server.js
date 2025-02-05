72cbc5538ff2e4b5cad12eca9106df9a344dec4f
Vien Dinh <maxvien@gmail.com>
2025-02-05 16:09:25 +0700

    qa: use-a-template-engines-powers

--- freecodecamp-quality-assurance/advanced-node-and-express/server.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
 const PORT = process.env.PORT || 3000;
