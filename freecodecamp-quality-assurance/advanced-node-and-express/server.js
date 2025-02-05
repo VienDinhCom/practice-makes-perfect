f9fe28e2c356d5eeb5b2c5921a960878e68feb25
Vien Dinh <maxvien@gmail.com>
2025-02-05 16:18:48 +0700

    qa: set-up-passport

--- freecodecamp-quality-assurance/advanced-node-and-express/server.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For fCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,11 +16,24 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route('/').get((req, res) => {
   res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
