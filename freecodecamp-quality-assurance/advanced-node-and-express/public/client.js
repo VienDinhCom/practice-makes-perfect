ca5ded3a9529a2d4343cc9188ffdd23338cbeeb4
Vien Dinh <maxvien@gmail.com>
2025-02-06 17:52:21 +0700

    qa: format code

--- freecodecamp-quality-assurance/advanced-node-and-express/public/client.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/public/client.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   /*global io*/
   let socket = io();
-  
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
