2eedafc52bd69a9c037e1c6096a2915e74dff165
Vien Dinh <maxvien@gmail.com>
2025-02-06 16:03:36 +0700

    qa: registration-of-new-users

--- freecodecamp-quality-assurance/advanced-node-and-express/server.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/server.js
@@ -41,6 +41,7 @@ myDB(async (client) => {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -75,9 +76,41 @@ myDB(async (client) => {
     res.redirect('/');
   }
 
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+  app.route('/login').post((req, res) => {
     res.redirect('/profile');
   });
+
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render('profile', { username: req.user.username });
