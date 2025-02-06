d0409dff32023c87113b338f55583eebff0af589
Vien Dinh <maxvien@gmail.com>
2025-02-06 17:47:36 +0700

    qa: set-up-the-environment

--- freecodecamp-quality-assurance/advanced-node-and-express/public/client.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/public/client.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  /*global io*/
+  let socket = io();
+  
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
