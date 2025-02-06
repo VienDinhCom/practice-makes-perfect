fdd8e899e75e472fb2f19515fdbf7ffa52dc19b4
Vien Dinh <maxvien@gmail.com>
2025-02-06 17:47:36 +0700

    qa: set-up-the-environment

--- freecodecamp-quality-assurance/advanced-node-and-express/routes.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/routes.js
@@ -1,67 +1,22 @@
-'use strict';
-require('dotenv').config();
-
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 module.exports = function (app, myDataBase) {
   app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
     res.render('index', {
       title: 'Connected to Database',
-      message: 'Please login',
+      message: 'Please log in',
       showLogin: true,
       showRegistration: true,
-      showSocialAuth: true,
+      showSocialAuth: true
     });
   });
 
-  app.route('/login').post((req, res) => {
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
 
-  app.route('/register').post(
-    (req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect('/');
-        } else {
-          const hash = bcrypt.hashSync(req.body.password, 12);
-
-          myDataBase.insertOne(
-            {
-              username: req.body.username,
-              password: hash,
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect('/');
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
-            }
-          );
-        }
-      });
-    },
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res, next) => {
-      res.redirect('/profile');
-    }
-  );
-
-  app.route('/auth/github').get(passport.authenticate('github', { failureRedirect: '/' }));
-
-  app
-    .route('/auth/github/callback')
-    .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res, next) => {
-      res.redirect('/profile');
-    });
-
-  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+  app.route('/profile').get(ensureAuthenticated, (req,res) => {
     res.render('profile', { username: req.user.username });
   });
 
@@ -70,14 +25,53 @@ module.exports = function (app, myDataBase) {
     res.redirect('/');
   });
 
+  app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: hash
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    req.session.user_id = req.user.id;
+    res.redirect("/chat");
+  })
+
   app.use((req, res, next) => {
-    res.status(404).type('text').send('Not Found');
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
-};
+}
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
-}
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+};
--- freecodecamp-quality-assurance/advanced-node-and-express/server.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/server.js
@@ -1,37 +1,53 @@
 'use strict';
 require('dotenv').config();
-
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
-const auth = require('./auth.js');
+const session = require('express-session');
+const passport = require('passport');
 const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
 
-fccTesting(app); // For fCC testing purposes
-
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-myDB(async (client) => {
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  auth(app, myDataBase);
   routes(app, myDataBase);
-}).catch((e) => {
+  auth(app, myDataBase);
+
+  io.on('connection', socket => {
+    console.log('A user has connected');
+  });
+  
+}).catch(e => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
-
+  
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
