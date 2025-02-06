3790277e15a8d1e0b4ce4fbb30ed7d59df79859d
Vien Dinh <maxvien@gmail.com>
2025-02-06 15:58:57 +0700

    qa: logging-a-user-out

--- freecodecamp-quality-assurance/advanced-node-and-express/server.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/server.js
@@ -82,6 +82,15 @@ myDB(async (client) => {
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render('profile', { username: req.user.username });
   });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
