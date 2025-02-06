fea58072923ffefd7cc22ade61ddb24f8f68c0a4
Vien Dinh <maxvien@gmail.com>
2025-02-06 16:29:21 +0700

    qa: hashing-your-passwords

--- freecodecamp-quality-assurance/advanced-node-and-express/server.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/server.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -63,7 +64,11 @@ myDB(async (client) => {
         console.log(`User ${username} attempted to log in.`);
         if (err) return done(err);
         if (!user) return done(null, false);
-        if (password !== user.password) return done(null, false);
+
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+
         return done(null, user);
       });
     })
@@ -88,10 +93,12 @@ myDB(async (client) => {
         } else if (user) {
           res.redirect('/');
         } else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
+
           myDataBase.insertOne(
             {
               username: req.body.username,
-              password: req.body.password,
+              password: hash,
             },
             (err, doc) => {
               if (err) {
