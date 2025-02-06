af9a359c135c59784c73743aa58a28b44f20064e
Vien Dinh <maxvien@gmail.com>
2025-02-06 17:39:17 +0700

    qa: implementation-of-social-authentication-iii

--- freecodecamp-quality-assurance/advanced-node-and-express/auth.js
+++ freecodecamp-quality-assurance/advanced-node-and-express/auth.js
@@ -59,6 +59,30 @@ module.exports = function (app, myDataBase) {
       function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
         //Database logic here with callback containing your user object
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || 'John Doe',
+              photo: profile.photos[0].value || '',
+              email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
+              created_on: new Date(),
+              provider: profile.provider || '',
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          }
+        );
       }
     )
   );
