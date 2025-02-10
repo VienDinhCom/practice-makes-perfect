342df0a6b225204de747bc2d55e3fb4b4e747927
Vien Dinh <maxvien@gmail.com>
2025-02-10 17:32:08 +0700

    qa: metric-imperial-converter

--- freecodecamp-quality-assurance/metric-imperial-converter/routes/api.js
+++ freecodecamp-quality-assurance/metric-imperial-converter/routes/api.js
@@ -4,7 +4,22 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', (req, res) => {
+    const input = req.query.input;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+
+    if (initNum === null && initUnit === null) return res.send('invalid number and unit');
+    if (initNum === null) return res.send('invalid number');
+    if (initUnit === null) return res.send('invalid unit');
+
+    console.log({ input, initNum, initUnit });
+
+    const result = convertHandler.convert(initNum, initUnit);
+
+    res.status(200).json(result);
+  });
 };
