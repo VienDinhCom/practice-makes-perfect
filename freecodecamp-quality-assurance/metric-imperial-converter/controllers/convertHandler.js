021a22600fa632dd81839a0a88f4731c112cba8f
Vien Dinh <maxvien@gmail.com>
2025-02-10 17:32:08 +0700

    qa: metric-imperial-converter

--- freecodecamp-quality-assurance/metric-imperial-converter/controllers/convertHandler.js
+++ freecodecamp-quality-assurance/metric-imperial-converter/controllers/convertHandler.js
@@ -1,44 +1,89 @@
+const convertUnits = {
+  gal: 'l',
+  l: 'gal',
+  mi: 'km',
+  km: 'mi',
+  lbs: 'kg',
+  kg: 'lbs',
+};
+
+const spellOutUnits = {
+  gal: 'gallons',
+  l: 'litters',
+  mi: 'miles',
+  km: 'kilometers',
+  lbs: 'pounds',
+  kg: 'kilograms',
+};
+
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  this.getNum = function (input = '') {
+    return Number(input.match(/^\d+(?:\/\/\d+)?(?=[a-zA-Z])/)[0]);
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (input) {
+    const unit = input.match(/[A-Za-z]+$/)[0].toLowerCase();
+
+    return spellOutUnits[unit] ? unit : null;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    return convertUnits[initUnit];
+  };
+
+  this.spellOutUnit = function (unit) {
+    return spellOutUnits[unit];
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+    const returnNum = (() => {
+      switch (initUnit) {
+        case 'gal':
+          return initNum * galToL;
+
+        case 'L':
+          return initNum / galToL;
+
+        case 'lbs':
+          return initNum * lbsToKg;
+
+        case 'kg':
+          return initNum / lbsToKg;
+
+        case 'mi':
+          return initNum * miToKm;
+
+        case 'km':
+          return initNum / miToKm;
+
+        default:
+          throw new Error('Input is invalid');
+      }
+    })();
+
+    const returnUnit = this.getReturnUnit(initUnit);
+    const string = this.getString(initNum, initUnit, returnNum, returnUnit);
+
+    // console.log({ initNum, initUnit, returnNum, returnUnit, string });
+
+    return {
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    };
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
-    return result;
+
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
-  
 }
 
 module.exports = ConvertHandler;
