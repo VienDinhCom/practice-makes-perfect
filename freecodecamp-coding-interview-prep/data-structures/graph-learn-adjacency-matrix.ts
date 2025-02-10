bdce7a9b1a936270c66caa48c6313291e5da87f9
Vien Dinh <maxvien@gmail.com>
2025-02-10 09:32:49 +0700

    review: dsa: graph: adjacency-matrix

--- freecodecamp-coding-interview-prep/data-structures/graph-learn-adjacency-matrix.ts
+++ freecodecamp-coding-interview-prep/data-structures/graph-learn-adjacency-matrix.ts
@@ -1,13 +1,5 @@
 // https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/adjacency-matrix
 
-const adjMatUndirected = [
-  [0, 0, 1, 1, 0],
-  [0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 1],
-  [1, 0, 0, 0, 1],
-  [0, 0, 1, 1, 0],
-];
-
 // const adjMatUndirected = [
 //   [0, 0, 0, 0, 0],
 //   [0, 0, 0, 0, 0],
@@ -15,3 +7,12 @@ const adjMatUndirected = [
 //   [0, 0, 0, 0, 0],
 //   [0, 0, 0, 0, 0],
 // ];
+
+// review
+const adjMatUndirected = [
+  [0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [0, 0, 1, 1, 0],
+];
