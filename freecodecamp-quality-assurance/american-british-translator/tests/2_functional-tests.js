71944c2c2db39fa01b5f86f9355d6a87437a4eab
Vien Dinh <maxvien@gmail.com>
2025-02-14 08:16:50 +0700

    qa: american-british-translator: functional tests

--- freecodecamp-quality-assurance/american-british-translator/tests/2_functional-tests.js
+++ freecodecamp-quality-assurance/american-british-translator/tests/2_functional-tests.js
@@ -5,8 +5,94 @@ const server = require('../server.js');
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
-
 suite('Functional Tests', () => {
+  test('Translation with text and locale fields: POST request to /api/translate', (done) => {
+    chai.request(server)
+      .post('/api/translate')
+      .send({
+        text: "I ate yogurt for breakfast.",
+        locale: "american-to-british"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        // Expected translation based on the translator should highlight "yoghurt"
+        assert.property(res.body, 'translation');
+        assert.equal(res.body.translation, "I ate <span class=\"highlight\">yoghurt</span> for breakfast.");
+        done();
+      });
+  });
 
+  test('Translation with text and invalid locale field: POST request to /api/translate', (done) => {
+    chai.request(server)
+      .post('/api/translate')
+      .send({
+        text: "I ate yogurt for breakfast.",
+        locale: "invalid-locale"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, "Invalid value for locale field");
+        done();
+      });
+  });
+
+  test('Translation with missing text field: POST request to /api/translate', (done) => {
+    chai.request(server)
+      .post('/api/translate')
+      .send({
+        locale: "american-to-british"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, "Required field(s) missing");
+        done();
+      });
+  });
+
+  test('Translation with missing locale field: POST request to /api/translate', (done) => {
+    chai.request(server)
+      .post('/api/translate')
+      .send({
+        text: "I ate yogurt for breakfast."
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, "Required field(s) missing");
+        done();
+      });
+  });
+
+  test('Translation with empty text: POST request to /api/translate', (done) => {
+    chai.request(server)
+      .post('/api/translate')
+      .send({
+        text: "",
+        locale: "american-to-british"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, "No text to translate");
+        done();
+      });
+  });
+
+  test('Translation with text that needs no translation: POST request to /api/translate', (done) => {
+    chai.request(server)
+      .post('/api/translate')
+      .send({
+        text: "Hello",
+        locale: "american-to-british"
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'translation');
+        // When no translation is needed, the API returns this message
+        assert.equal(res.body.translation, "Everything looks good to me!");
+        done();
+      });
+  });
 });
