29a0d34ec227245af1c1867eb8f6f4f1166997fd
Vien Dinh <maxvien@gmail.com>
2025-02-17 10:44:11 +0700

    reivew: dsa: insertion-sort

--- freecodecamp-coding-interview-prep/algorithms/sorting/insertion-sort.ts
+++ freecodecamp-coding-interview-prep/algorithms/sorting/insertion-sort.ts
@@ -1,24 +1,20 @@
 import { expect } from 'jsr:@std/expect';
 
 function insertionSort(nums: number[]): number[] {
-  if (nums.length <= 1) return nums;
+  if (nums.length <= 0) return nums;
 
-  for (let i = 1; i < nums.length; i++) {
-    const tmp = nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    const temp = nums[i];
 
     let j = i - 1;
 
-    while (j >= 0) {
-      if (nums[j] > tmp) {
-        nums[j + 1] = nums[j];
-      } else {
-        break;
-      }
+    while (j >= 0 && temp < nums[j]) {
+      nums[j + 1] = nums[j];
 
       j--;
     }
 
-    nums[j + 1] = tmp;
+    nums[j + 1] = temp;
   }
 
   return nums;
