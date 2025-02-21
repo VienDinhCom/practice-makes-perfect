17659754aef7c708607cb863863df373cdbbce0c
Vien Dinh <maxvien@gmail.com>
2025-02-21 17:42:11 +0700

    visualize-data-with-a-scatterplot-graph: use time for y scale

--- freecodecamp-data-visualization/projects/scatterplot-graph/scatterplot-graph.ts
+++ freecodecamp-data-visualization/projects/scatterplot-graph/scatterplot-graph.ts
@@ -5,21 +5,17 @@ import * as d3 from 'd3';
     interface Data {
       name: string;
       year: number;
-      date: Date;
-      time: string;
-      seconds: number;
+      time: Date;
       doping?: string;
     }
 
     const data: Data[] = await d3
       .json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json')
       .then((data: any) =>
-        data.map(({ Name, Year, Seconds, Time, Doping }: any) => ({
+        data.map(({ Name, Year, Seconds, Doping }: any) => ({
           name: Name,
           year: Year,
-          date: new Date(`${Year}-01-01`),
-          time: Time,
-          seconds: Seconds,
+          time: new Date(0, 0, 0, 0, 0, Seconds),
           doping: Doping,
         }))
       );
@@ -50,34 +46,25 @@ import * as d3 from 'd3';
       .append('g')
       .call(xAxis)
       .attr('id', 'x-axis')
-      .attr('transform', `translate(${padding}, ${height + padding})`);
+      .attr('transform', `translate(0, ${height + padding})`);
 
     /* Y Axis
     =========================================================================*/
-    const yMin = d3.min(data, ({ seconds }) => seconds)!;
-    const yMax = d3.max(data, ({ seconds }) => seconds)!;
+    const yMin = d3.min(data, ({ time }) => time)!;
+    const yMax = d3.max(data, ({ time }) => time)!;
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([yMin - 60, yMax + 60])
-      .range([height, 0]);
+    const yScale = d3.scaleTime().domain([yMax, yMin]).range([height, 0]);
 
     const yAxis = d3.axisLeft(yScale).tickFormat((seconds) => {
       const mins = Math.floor((seconds as number) / 60);
-      const secs = (seconds as number) - mins * 60;
-
+      const secs = (seconds as number) % 60;
       return `${mins}:${String(secs).padStart(2, '0')}`;
     });
 
-    chart
-      .append('g')
-      .call(yAxis)
-      .attr('id', 'y-axis')
-      .attr('transform', `translate(${padding * 2}, ${padding})`);
+    chart.append('g').call(yAxis).attr('id', 'y-axis').attr('transform', `translate(${padding}, ${padding})`);
 
     /* Dots 
     =========================================================================*/
-
     chart
       .selectAll('.dot')
       .data(data)
@@ -85,21 +72,11 @@ import * as d3 from 'd3';
       .append('circle')
       .attr('class', 'dot')
       .attr('r', 6)
-      .attr('cx', function ({ year }) {
-        return padding + xScale(year);
-      })
-      .attr('cy', function ({ seconds }) {
-        return padding + yScale(seconds);
-      })
-      .attr('data-xvalue', function ({ year }) {
-        return year;
-      })
-      .attr('data-yvalue', function ({ time }) {
-        return time;
-      })
-      .style('fill', function ({ doping }) {
-        return doping ? 'red' : 'green';
-      });
+      .attr('cx', ({ year }) => xScale(year))
+      .attr('cy', ({ time }) => yScale(time) + padding)
+      .attr('data-xvalue', ({ year }) => year)
+      .attr('data-yvalue', ({ time }) => time.toISOString())
+      .style('fill', ({ doping }) => (doping ? 'red' : 'green'));
   } catch (error) {
     console.error(error);
   }
