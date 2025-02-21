d6beea681ab84b971aa4ecf4932f69239137d96f
Vien Dinh <maxvien@gmail.com>
2025-02-21 16:29:38 +0700

    data-visualization: scatterplot-graph: y axis

--- freecodecamp-data-visualization/projects/scatterplot-graph/scatterplot-graph.ts
+++ freecodecamp-data-visualization/projects/scatterplot-graph/scatterplot-graph.ts
@@ -51,6 +51,28 @@ import * as d3 from 'd3';
 
     /* Y Axis
     =========================================================================*/
+    const seconds = data.map(({ Seconds }) => Seconds);
+
+    const yMin = d3.min(seconds)!;
+    const yMax = d3.max(seconds)!;
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([yMin - 60, yMax + 60])
+      .range([height, 0]);
+
+    const yAxis = d3.axisLeft(yScale).tickFormat((seconds) => {
+      const mins = Math.floor((seconds as number) / 60);
+      const secs = (seconds as number) - mins * 60;
+
+      return `${mins}:${String(secs).padStart(2, '0')}`;
+    });
+
+    chart
+      .append('g')
+      .call(yAxis)
+      .attr('id', 'y-axis')
+      .attr('transform', `translate(${padding * 2}, ${padding})`);
 
     /* Bars 
     =========================================================================*/
