1386b08e4328f665b3de0d59d0dd2f5af743353d
Vien Dinh <maxvien@gmail.com>
2025-02-22 10:20:09 +0700

    frontend: drum-machine: update path

--- freecodecamp-front-end-development-libraries/vite.config.ts
+++ freecodecamp-front-end-development-libraries/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         quote: resolve(__dirname, 'projects/random-quote-machine/random-quote-machine.html'),
         markdown: resolve(__dirname, 'projects/markdown-previewer/markdown-previewer.html'),
-        drum: resolve(__dirname, 'drum-machine.html'),
+        drum: resolve(__dirname, 'projects/drum-machine/drum-machine.html'),
         calculator: resolve(__dirname, 'javascript-calculator.html'),
         clock: resolve(__dirname, '25--5-clock.html'),
       },
