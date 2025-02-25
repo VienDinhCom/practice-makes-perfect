57f8d7a9e11f25e4a4de43322af60bf461970778
Vien Dinh <maxvien@gmail.com>
2025-02-25 16:05:21 +0700

    visualize-data-with-a-heat-map: legend

--- freecodecamp-data-visualization/projects/heat-map/heat-map.ts
+++ freecodecamp-data-visualization/projects/heat-map/heat-map.ts
@@ -19,7 +19,7 @@ import * as d3 from 'd3';
 
     const width = 800;
     const height = 400;
-    const padding = 60;
+    const padding = 100;
 
     const chart = d3
       .select('#chart')
@@ -94,6 +94,64 @@ import * as d3 from 'd3';
 
         return `${d3.timeFormat('%Y - %B')(date)}\n${temp.toFixed(2)}°C\n${d.variance.toFixed(2)}°C`;
       });
+
+    /* Legend 
+  =========================================================================*/
+    const legendWidth = 300;
+
+    const minTemp = d3.min(monthlyVariance, (d) => baseTemperature + d.variance) || 0;
+    const maxTemp = d3.max(monthlyVariance, (d) => baseTemperature + d.variance) || 10;
+
+    const legendThreshold = d3
+      .scaleThreshold<number, string>()
+      .domain(
+        (() => {
+          const step = (maxTemp - minTemp) / 5;
+          return [minTemp + step, minTemp + step * 2, minTemp + step * 3, minTemp + step * 4];
+        })()
+      )
+      .range([
+        d3.interpolateRdYlBu(0.9), // Cold (blue)
+        d3.interpolateRdYlBu(0.7), // Cool (light blue)
+        d3.interpolateRdYlBu(0.5), // Moderate (yellow)
+        d3.interpolateRdYlBu(0.3), // Warm (orange)
+        d3.interpolateRdYlBu(0.1), // Hot (red)
+      ]);
+
+    const legendXScale = d3.scaleLinear().domain([minTemp, maxTemp]).range([0, legendWidth]);
+
+    const legendAxis = d3
+      .axisBottom(legendXScale)
+      .tickSize(10)
+      .tickFormat((d) => `${d.valueOf().toFixed(1)}°C`)
+      .tickValues(legendThreshold.domain().concat([minTemp, maxTemp]));
+
+    const legend = chart
+      .append('g')
+      .attr('id', 'legend')
+      .attr('transform', `translate(${padding}, ${height + padding + 50})`);
+
+    legend
+      .selectAll('rect')
+      .data(
+        legendThreshold.range().map((color, i) => {
+          const d = legendThreshold.domain();
+          return {
+            color: color,
+            x0: i === 0 ? minTemp : d[i - 1],
+            x1: i === legendThreshold.range().length - 1 ? maxTemp : d[i],
+          };
+        })
+      )
+      .enter()
+      .append('rect')
+      .attr('x', (d) => legendXScale(d.x0))
+      .attr('y', 0)
+      .attr('width', (d) => legendXScale(d.x1) - legendXScale(d.x0))
+      .attr('height', 20)
+      .attr('fill', (d) => d.color);
+
+    legend.append('g').call(legendAxis).attr('transform', `translate(0, 20)`);
   } catch (error) {
     console.error(error);
   }
