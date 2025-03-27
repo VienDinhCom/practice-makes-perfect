c1767381eec78a2f2d70534ca5d64c7036725f5c
Vien Dinh <maxvien@gmail.com>
2025-03-27 01:54:55 +0000

    review: javascript: palindrome-checker

--- freecodecamp-javascript-algorithms-and-data-structures/projects/palindrome-checker.ts
+++ freecodecamp-javascript-algorithms-and-data-structures/projects/palindrome-checker.ts
@@ -1,12 +1,11 @@
-// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
-
 import { expect } from 'jsr:@std/expect';
 
-// review
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+
 function palindrome(str: string) {
   const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-  return cleanedStr.split('').reverse().join('') === cleanedStr;
+  return cleanedStr === cleanedStr.split('').reverse().join('');
 }
 
 Deno.test('palindrome should return a boolean', () => {
