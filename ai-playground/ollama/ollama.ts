66cfde3da7723755ee3c41ec0284dd2cb5d3ba9d
Vien Dinh <maxvien@gmail.com>
2025-03-28 01:20:31 +0000

    ai: ollama: stream out

--- ai-playground/ollama/ollama.ts
+++ ai-playground/ollama/ollama.ts
@@ -3,6 +3,35 @@
 import ollama from 'ollama';
 
 (async () => {
+  const prompt = 'Tell a story about AI.';
+
+  console.log('\n Asking: ' + prompt + '\n');
+
+  const streamResponse = await ollama.chat({
+    model: 'gemma3:1b',
+    messages: [
+      {
+        role: 'system',
+        content: 'You are a storyteller.',
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    stream: true,
+  });
+
+  for await (const chunk of streamResponse) {
+    const content = chunk.message.content;
+
+    // process.stdout.write(content);
+
+    Deno.stdout.write(new TextEncoder().encode(content));
+  }
+})();
+
+(async () => {
   const prompt = 'Explain quantum computing in simple terms.';
 
   console.log('\n Asking: ' + prompt + '\n');
