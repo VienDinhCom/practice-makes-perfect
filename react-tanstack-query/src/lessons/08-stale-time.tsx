ccaba0d422599e71327aafda090143a6df697167
Vien Dinh <maxvien@gmail.com>
2025-08-02 04:56:16 +0000

    react-query: stale time

---
+++ react-tanstack-query/src/lessons/08-stale-time.tsx
@@ -0,0 +1,54 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import axios from "axios";
+import { shuffle } from "es-toolkit";
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Pokemon />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+interface Data {
+  name: string;
+}
+
+function Pokemon() {
+  const queryInfo = useQuery({
+    queryKey: ["pokemon"],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      const pokemons = await axios
+        .get("https://pokeapi.co/api/v2/pokemon")
+        .then((res) => res.data.results as Data[]);
+
+      return shuffle(pokemons);
+    },
+    refetchOnWindowFocus: true,
+    staleTime: 5000, // Default is 0
+  });
+
+  return queryInfo.isLoading ? (
+    "Loading..."
+  ) : queryInfo.isError ? (
+    queryInfo.error?.message
+  ) : (
+    <div>
+      {queryInfo.isFetching && <div>Updatding...</div>}
+      <br />
+      {queryInfo.data?.map((result) => (
+        <div key={result.name}>{result.name}</div>
+      ))}
+    </div>
+  );
+}
--- react-tanstack-query/src/main.tsx
+++ react-tanstack-query/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./lessons/07-refetching-indicators.tsx";
+import { App } from "./lessons/08-stale-time.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
