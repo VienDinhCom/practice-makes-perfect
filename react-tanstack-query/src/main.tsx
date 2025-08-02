115007fc0d2046f1e3f1ad0438eedbba2ce02ab9
Vien Dinh <maxvien@gmail.com>
2025-08-02 04:11:30 +0000

    react-query: devtools

--- react-tanstack-query/src/main.tsx
+++ react-tanstack-query/src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { App } from "./lessons/05-devtools.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
