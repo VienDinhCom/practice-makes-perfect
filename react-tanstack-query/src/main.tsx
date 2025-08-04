00afa64476624026e789863e9eab44a96d7d8e75
Vien Dinh <maxvien@gmail.com>
2025-08-04 04:44:44 +0000

    react-query: dependent queries

--- react-tanstack-query/src/main.tsx
+++ react-tanstack-query/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./lessons/17-query-cancellation.tsx";
+import { App } from "./lessons/18-dependent-queries.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
