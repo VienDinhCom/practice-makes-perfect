0e6c59607b7a6f8b6f7d2e3153099b5b61f71434
Vien Dinh <maxvien@gmail.com>
2025-08-10 03:37:37 +0000

    react-query: config project

--- react-tanstack-query/vite.config.ts
+++ react-tanstack-query/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     tanstackRouter({
       target: "react",
