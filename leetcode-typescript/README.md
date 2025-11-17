e1f369f1fc28934670ef0ecd8131032f7192cf41
Vien Dinh <maxvien@gmail.com>
2025-11-17 11:19:49 +0700

    leetcode: two sum

--- leetcode-typescript/README.md
+++ leetcode-typescript/README.md
@@ -1,3 +1,9 @@
 # LeetCode TypeScript
 
 https://www.techinterviewhandbook.org/grind75/?hours=8&weeks=8
+
+## Problems
+
+### Array
+
+- [Two Sum](problems/001-two-sum.ts)
--- leetcode-typescript/problems/001-two-sum.ts
+++ leetcode-typescript/problems/001-two-sum.ts
@@ -1,5 +1,7 @@
 import { expect } from "jsr:@std/expect";
 
+// https://leetcode.com/problems/two-sum/
+
 function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
