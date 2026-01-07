2a871781f99513d9362be079c8abc8c3112c6918
Vien Dinh <maxvien@gmail.com>
2026-01-07 16:48:37 +0700

    svelte-kit: auto-assign admin role to the first registered user upon signup

--- svelte-kit/src/lib/auth.ts
+++ svelte-kit/src/lib/auth.ts
@@ -1,7 +1,9 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '$lib/server/db';
-import { admin } from 'better-auth/plugins';
+import { user } from '$lib/server/db/schema';
+import { admin, createAuthMiddleware } from 'better-auth/plugins';
+import { count, eq } from 'drizzle-orm';
 
 export const auth = betterAuth({
 	emailAndPassword: {
@@ -10,5 +12,21 @@ export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'pg'
 	}),
-	plugins: [admin()]
+	plugins: [admin()],
+	hooks: {
+		after: createAuthMiddleware(async (ctx) => {
+			if (ctx.path.startsWith('/sign-up')) {
+				const newSession = ctx.context.newSession;
+
+				if (newSession) {
+					// Check if this is the first user
+					const [result] = await db.select({ count: count() }).from(user);
+
+					if (result.count === 1) {
+						await db.update(user).set({ role: 'admin' }).where(eq(user.id, newSession.user.id));
+					}
+				}
+			}
+		})
+	}
 });
